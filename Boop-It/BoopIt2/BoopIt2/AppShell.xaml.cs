97a373930742f4f580629f0ccc732b1f01ab195f
Jordan Kidwell <jkid2023@gmail.com>
2021-03-02 19:11:20 -0800

    added in quite a bit of stuff!

---
+++ Boop-It/BoopIt2/BoopIt2/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using BoopIt2.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BoopIt2
+{
+  public partial class AppShell : Xamarin.Forms.Shell
+  {
+    public AppShell()
+    {
+      InitializeComponent();
+    }
+
+  }
+}
---
+++ Boop-It/BoopIt2/BoopIt2/Views/NotesPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BoopIt2.Views
+{
+  public partial class NotesPage : ContentPage
+  {
+    string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+    public NotesPage()
+    {
+      InitializeComponent();
+
+      // Read the file.
+      if (File.Exists(_fileName))
+      {
+        editor.Text = File.ReadAllText(_fileName);
+      }
+    }
+
+    void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+      // Save the file.
+      File.WriteAllText(_fileName, editor.Text);
+    }
+
+    void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+      // Delete the file.
+      if (File.Exists(_fileName))
+      {
+        File.Delete(_fileName);
+      }
+      editor.Text = string.Empty;
+    }
+  }
+}
